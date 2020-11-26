1c3b06d71b92c862fbcef0d0ff2846b393b54541
a-haruta <a-haruta@colopl.co.jp>
2020-11-26 12:09:04 +0900

    Yubaba

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Yubaba
+{
+    class Yubaba
+    {
+        static unsafe void Main(string[] args)
+        {
+            Console.WriteLine("契約書だよ。そこに名前を書きな。");
+            var name = Console.ReadLine();
+            Console.WriteLine($"フン。{name}というのかい。贅沢な名だねぇ。");
+
+            var random = new Random();
+            var newNameIndex = random.Next(name.Length);
+			GCHandle hName = GCHandle.Alloc(name, GCHandleType.Pinned);
+			char * pName = (char *)hName.AddrOfPinnedObject();
+            char newName = pName[newNameIndex];
+			string value = $"今からお前の名前は{newName}だ。いいかい、{newName}だよ。分かったら返事をするんだ、{newName}!!";
+			Console.WriteLine(value);
+        }
+    }
+}
